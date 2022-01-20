29fc304c73ab881c98e635db633019093627dcef
w.benarto <w.benarto@gmail.com>
2022-01-19 19:01:41 -0800

    dependency error

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core'
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
-import Rating from '@material-ui/lab/rating'
+import Rating from '@material-ui/lab/Rating'
 
 import mapStyles from './mapStyles'
 import useStyles from './styles.js'
--- components/PlaceDetails/PlaceDetails.jsx
+++ components/PlaceDetails/PlaceDetails.jsx
@@ -1,7 +1,7 @@
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core'
 import LocationOnIcon from '@material-ui/icons/LocationOn'
 import PhoneIcon from '@material-ui/icons/Phone'
-import Rating from '@material-ui/lab/rating'
+import Rating from '@material-ui/lab/Rating'
 
 import useStyles from './styles'
 
