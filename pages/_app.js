eb932b8b6634176df9bd4bf323b3d9b903fbedad
w.benarto <w.benarto@gmail.com>
2022-07-14 13:55:48 -0700

    gscript

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import '../styles/globals.css'
 import Script from 'next/script'
-import GooglePlacesScript from "../components/GooglePlacesScript";
+
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <GooglePlacesScript /> */}
-     
       {/* <Head>
         <script src={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`} />
       </Head> */}
