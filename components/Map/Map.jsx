313e726588191a5213af1f1fe36a7065561ed42b
w.benarto <w.benarto@gmail.com>
2022-07-14 13:45:03 -0700

    google script

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -18,9 +18,6 @@ const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, 
 
     return (
         <>
-            <Head>
-                <script src={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`} />
-            </Head>
 
             <div className={classes.mapContainer}>
 
